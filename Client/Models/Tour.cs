a5e092c4d14ff776563e0fd34041ea1472c861ec
Markus Rösner <markus.roesner@gmx.net>
2024-05-08 22:28:15 +0200

    work in progress

--- Client/Models/Tour.cs
+++ Client/Models/Tour.cs
@@ -1,13 +1,12 @@
 public class Tour
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public String From { get; set; }
-    public String To { get; set; }
-    public double EstimatedTime { get; set; }
-    public string Description { get; set; }
-    public string TransportType { get; set; }
-    public int Difficulty { get; set; }
-
-    public int Rating { get; set; }
+    public string Name { get; set; } = "";
+    public String From { get; set; } = "";
+    public String To { get; set; } = "";
+    public double EstimatedTime { get; set; } = 0;
+    public string Description { get; set; } = "";
+    public string TransportType { get; set; } = "";
+    public int Difficulty { get; set; } = 0;
+    public int Rating { get; set; } = 0;
 }
