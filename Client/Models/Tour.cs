0e895e0c6f5c2c181a33cf87b1e4c6500afbf956
Markus Rösner <markus.roesner@gmx.net>
2024-05-08 17:35:27 +0200

    work in progress

--- Client/Models/Tour.cs
+++ Client/Models/Tour.cs
@@ -2,9 +2,12 @@ public class Tour
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public String From { get; set; }
+    public String To { get; set; }
+    public double EstimatedTime { get; set; }
     public string Description { get; set; }
     public string TransportType { get; set; }
-    public string From { get; set; }
-    public string To { get; set; }
-    public int Popularity { get; set; }
+    public int Difficulty { get; set; }
+
+    public int Rating { get; set; }
 }
--- Client/ViewModels/TourDetailsPageViewModel.cs
+++ Client/ViewModels/TourDetailsPageViewModel.cs
@@ -6,20 +6,20 @@ public class TourDetailsPageViewModel
 {
     public Tour Tour { get; private set; }
 
- private readonly Tour[] Tours = new Tour[]
-    {
-        new Tour { Id=1, Name = "Waldviertel", Description = "Waldviertel Tour", TransportType = "Bike", From = "Wien", To = "Waldviertel", Popularity = 5},
-        new Tour { Id=2, Name = "Donauradweg", Description = "Donauradweg Tour", TransportType = "Bike", From = "Passau", To = "Wien", Popularity = 4},
-        new Tour { Id=3, Name = "Alpe Adria", Description = "Alpe Adria Tour", TransportType = "Bike", From = "Salzburg", To = "Adria", Popularity = 3},
-        new Tour { Id=4, Name = "Grossglockner", Description = "Grossglockner Tour", TransportType = "Bike", From = "Salzburg", To = "Grossglockner", Popularity = 2},
-        new Tour { Id=5, Name = "Neusiedlersee", Description = "Neusiedlersee Tour", TransportType = "Bike", From = "Wien", To = "Neusiedlersee", Popularity = 1}
-    };
+    private readonly Tour[] Tours = new Tour[]
+       {
+        new Tour { Id=1, Name = "Waldviertel", Description = "Waldviertel Tour", TransportType = "Bike", From = "Wien", To = "Waldviertel", Rating = 5},
+        new Tour { Id=2, Name = "Donauradweg", Description = "Donauradweg Tour", TransportType = "Bike", From = "Passau", To = "Wien", Rating = 4},
+        new Tour { Id=3, Name = "Alpe Adria", Description = "Alpe Adria Tour", TransportType = "Bike", From = "Salzburg", To = "Adria", Rating = 3},
+        new Tour { Id=4, Name = "Grossglockner", Description = "Grossglockner Tour", TransportType = "Bike", From = "Salzburg", To = "Grossglockner", Rating = 2},
+        new Tour { Id=5, Name = "Neusiedlersee", Description = "Neusiedlersee Tour", TransportType = "Bike", From = "Wien", To = "Neusiedlersee", Rating = 1}
+       };
 
     public async Task InitializeAsync(int id)
     {
         await Task.Delay(500);
         // statt delay api call an backend
-        
+
         Tour = Tours.FirstOrDefault(t => t.Id == id);
 
     }
