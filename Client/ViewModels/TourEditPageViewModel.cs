d5399729ec57ab08642228f3dc69d3546a65b3ab
Markus Rösner <markus.roesner@gmx.net>
2024-05-09 19:58:25 +0200

    edit tour page

---
+++ Client/ViewModels/TourEditPageViewModel.cs
@@ -0,0 +1,34 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Client.Pages;
+
+public class TourEditPageViewModel
+{
+    public Tour Tour { get; private set; } = new Tour();
+
+    public void UpdateTour()
+    {
+        Console.WriteLine("UpdateTour", Tour.Name);
+    }
+
+    public async Task InitializeAsync(int id)
+    {
+        await Task.Delay(500);
+        // statt delay api call an backend
+
+        Tour = new Tour
+        {
+            Id = id,
+            Name = "Tour " + id,
+            From = "From " + id,
+            To = "To " + id,
+            EstimatedTime = id * 1.5,
+            Description = "Description " + id,
+            TransportType = "TransportType " + id,
+            Difficulty = id % 5,
+            Rating = id % 5
+        };
+
+    }
+
+}
