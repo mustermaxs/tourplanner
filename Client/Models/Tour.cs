58bde6e40d53a45e116311a1ed622e6bc7b28e42
Markus Rösner <markus.roesner@gmx.net>
2024-05-10 10:25:28 +0200

    changed rating and difficulty float, use TransportType Enum

--- Client/Models/Tour.cs
+++ Client/Models/Tour.cs
@@ -1,12 +1,20 @@
+public enum TransportType
+{
+    Car,
+    Bicycle,
+    Walking,
+    PublicTransport
+}
+
 public class Tour
 {
     public int Id { get; set; }
     public string Name { get; set; } = "";
-    public String From { get; set; } = "";
-    public String To { get; set; } = "";
+    public string From { get; set; } = "";
+    public string To { get; set; } = "";
     public double EstimatedTime { get; set; } = 0;
     public string Description { get; set; } = "";
-    public string TransportType { get; set; } = "";
-    public int Difficulty { get; set; } = 0;
-    public int Rating { get; set; } = 0;
+    public float Difficulty { get; set; } = 0;
+    public float Rating { get; set; } = 0;
+    public TransportType Transport { get; set; } = TransportType.Car;
 }
