3a98fc318c34e8d3718182043a4965693b1512ca
Markus Rösner <markus.roesner@gmx.net>
2024-05-10 19:24:56 +0200

    Add Tour Log Page

--- Client/Models/Tour.cs
+++ Client/Models/Tour.cs
@@ -9,7 +9,7 @@ public enum TransportType
 public class Tour
 {
     public int Id { get; set; }
-    public string Name { get; set; } = "";
+    public string Name { get; set; } = "Wanderweg";
     public string From { get; set; } = "";
     public string To { get; set; } = "";
     public double EstimatedTime { get; set; } = 0;
