1dfc5ff0d8cb7a9a22335326e34e1c607cdec0f0
Markus Rösner <markus.roesner@gmx.net>
2024-05-11 23:58:06 +0200

    Fetch tour from backend

--- Client/ViewModels/TourDetailsPageViewModel.cs
+++ Client/ViewModels/TourDetailsPageViewModel.cs
@@ -5,12 +5,12 @@ using System.Collections.Generic;
 public class TourDetailsPageViewModel
 {
     private IHttpService _httpService;
-    public TourDetailsPageViewModel(IHttpService httpServus)
+    public TourDetailsPageViewModel(IHttpService httpService)
     {
-        _httpService = httpServus;
+        _httpService = httpService;
     }
 
-    public Tour Tour { get; private set; }
+    public Tour Tour { get; private set; } 
     public List<TourLog> TourLogs { get; private set; } = [
         new TourLog { Id = 1, Date = new DateTime(2024, 6, 1), Comment = "Nice tour", Difficulty = 3, Duration = 3f, Rating = 5},
         new TourLog { Id = 3, Date = new DateTime(2024, 6, 3), Comment = "Awesome tour", Difficulty = 5, Duration = 9f, Rating = 5},
@@ -20,18 +20,19 @@ public class TourDetailsPageViewModel
         new TourLog { Id = 7, Date = new DateTime(2024, 6, 7), Comment = "Nice tour", Difficulty = 3, Duration = 4f, Rating = 4},
     ];
 
-    private readonly Tour[] Tours = new Tour[]
-       {
-        new Tour { Id=2, Name = "Waldviertel", Description = "Waldviertel Tour", TransportType = TransportType.Bicycle, From = "Wien", To = "Waldviertel", Popularity = 5, ChildFriendliness = 2, Distance = 200, EstimatedTime = 60},
-        new Tour { Id=9, Name = "Donauradweg", Description = "Donauradweg Tour", TransportType = TransportType.Bicycle, From = "Passau", To = "Wien", Popularity = 4,ChildFriendliness = 3, Distance = 300, EstimatedTime = 90},
-        new Tour { Id=11, Name = "Alpe Adria", Description = "Alpe Adria Tour", TransportType = TransportType.Bicycle, From = "Salzburg", To = "Adria", Popularity = 3, ChildFriendliness = 4, Distance = 400, EstimatedTime = 120},
-       };
-    
-
-    public async Task InitializeAsync(int tourId)
+    public async Task GetTour(int tourId)
     {
-        await Task.Delay(500); // Mock delay for async operation
-        Tour = Tours.FirstOrDefault(t => t.Id == tourId);
+        try
+        {
+            Tour = await _httpService.Get<Tour>($"Tours/{tourId}");
+            Console.WriteLine(Tour.Name);
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error fetching tours: {ex.Message}");
+            throw;
+        }
     }
 
     public async Task DeleteTour()
