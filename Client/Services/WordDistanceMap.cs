05dfd5892c5b74cd57afd476c7534d8d67dbdde3
Maximilian Sinnl <maximilian.sinnl@cssteam.at>
2024-05-28 16:43:12 +0200

    Add Fuzzy Matching Service

---
+++ Client/Services/WordDistanceMap.cs
@@ -0,0 +1,16 @@
+﻿namespace Client.Services.Search
+{
+    public class WordDistanceMap
+    {
+        public string Content { get; set; }
+        public int Distance { get; set; }
+        public bool IsPrefix { get; set; }
+
+        public WordDistanceMap(string content, int distance, bool isPrefix)
+        {
+            Content = Content;
+            Distance = distance;
+            IsPrefix = isPrefix;
+        }
+    };
+}
---
+++ UnitTests/Services/StringSearchServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Client.Services.Search;
+
+namespace UnitTests.Services;
+
+[TestFixture]
+[TestOf(typeof(StringSearchService))]
+public class StringSearchServiceTest
+{
+    private ISearchService SearchService;
+    
+    [SetUp]
+    public void Setup()
+    {
+        SearchService = new StringSearchService();
+    }
+
+    [Test]
+    public void Returns_Correct_Matches()
+    {
+        List<string> searchableWords = ["asmel", "esel", "bmsel"];
+        var matches = SearchService.GetMatches("amsel", searchableWords);
+
+        Assert.That(matches.All(match => searchableWords.Contains(match)));
+    }
+}
